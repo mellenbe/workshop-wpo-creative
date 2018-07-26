6f52465c1410d5b1a8563ee836cecaad7a8bfb5c
Sarah Kaindl <skaindl@users.noreply.github.com>
2018-07-26 13:26:10 +0200

    Fix minification of creative.js

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,8 @@ var banner = ['/*!\n',
 // Minify JavaScript
 gulp.task('js:minify', function() {
     return gulp.src([
+        './js/*.js',
+        '!./js/*.min.js',
         './dist/js/*.js',
         '!./dist/js/*.min.js'
     ])
