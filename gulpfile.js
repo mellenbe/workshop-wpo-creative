1c86222c64272b2923be53b6550c9796e3311838
Sarah Kaindl <skaindl@users.noreply.github.com>
2018-07-26 11:21:20 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
+var rollup = require('rollup');
 var browserSync = require('browser-sync').create();
 
 // Set the banner content
@@ -16,16 +18,16 @@ var banner = ['/*!\n',
   ''
 ].join('');
 
-// Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
+// Copy third party css from /node_modules into /vendor
+gulp.task('vendor:css', function() {
 
   // Bootstrap
   gulp.src([
-      './node_modules/bootstrap/dist/**/*',
+      './node_modules/bootstrap/dist/{css,css/*}',
       '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
       '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
     ])
-    .pipe(gulp.dest('./dist/vendor/bootstrap'))
+    .pipe(gulp.dest('./dist/vendor/bootstrap'));
 
   // Font Awesome
   gulp.src([
@@ -35,35 +37,45 @@ gulp.task('vendor', function() {
       '!./node_modules/font-awesome/.*',
       '!./node_modules/font-awesome/*.{txt,json,md}'
     ])
-    .pipe(gulp.dest('./dist/vendor/font-awesome'))
-
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./dist/vendor/jquery'))
-
-  // jQuery Easing
-  gulp.src([
-      './node_modules/jquery.easing/*.js'
-    ])
-    .pipe(gulp.dest('./dist/vendor/jquery-easing'))
+    .pipe(gulp.dest('./dist/vendor/font-awesome'));
 
   // Magnific Popup
   gulp.src([
-      './node_modules/magnific-popup/dist/*'
+      './node_modules/magnific-popup/dist/*.css'
     ])
-    .pipe(gulp.dest('./dist/vendor/magnific-popup'))
-
-  // Scrollreveal
-  gulp.src([
-      './node_modules/scrollreveal/dist/*.js'
-    ])
-    .pipe(gulp.dest('./dist/vendor/scrollreveal'))
+    .pipe(gulp.dest('./dist/vendor/magnific-popup'));
 
 });
 
+// Minify JavaScript
+gulp.task('js:minify', function() {
+    return gulp.src([
+        './dist/js/*.js',
+        '!./dist/js/*.min.js'
+    ])
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('js:concat', function() {
+    return gulp.src([
+        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+        './node_modules/jquery/dist/jquery.min.js',
+        './node_modules/jquery.easing/jquery.easing.min.js',
+        './node_modules/magnific-popup/dist/jquery.magnific-popup.min.js',
+        './node_modules/scrollreveal/dist/scrollreveal.min.js',
+        './dist/js/creative.min.js'
+    ])
+        .pipe(concat('all-scripts.min.js'))
+        .pipe(gulp.dest('./dist/js'));
+});
+
+// JS
+gulp.task('js', ['js:minify', 'js:concat']);
 
 gulp.task('html', function() {
   return gulp.src('./index.html')
@@ -95,25 +107,8 @@ gulp.task('css', function() {
 // CSS
 //gulp.task('css', ['css:minify','css:compile']);
 
-// Minify JavaScript
-gulp.task('js:minify', function() {
-  return gulp.src([
-      './dist/js/*.js',
-      '!./dist/js/*.min.js'
-    ])
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./dist/js'))
-    .pipe(browserSync.stream());
-});
-
-// JS
-gulp.task('js', ['js:minify']);
-
 // Default task
-gulp.task('default', ['html', 'css', 'js', 'images', 'vendor']);
+gulp.task('default', ['html', 'css', 'js', 'images', 'vendor:css']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -121,7 +116,7 @@ gulp.task('browserSync', function() {
     startPath: "/dist",
       server: {
         baseDir: '.'
-      },
+      }
   });
 });
 
